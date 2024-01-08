1b8ad95f58ea43e1bc97368710044e40380e5509
Team7419 <126924979+Team7419@users.noreply.github.com>
2024-01-08 12:43:55 -0800

    deleted + vision
    Co-Authored-By: Kaival Shah <63321599+KaivalSShah@users.noreply.github.com>
    Co-Authored-By: Jahaanshah Sheikh <113569013+Jolteous@users.noreply.github.com>

--- src/main/java/frc/robot/constants/BalanceConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.constants;
-
-public final class BalanceConstants {
-  public static double pitchDerivativeThreshold = 15;
-  public static double slowPower = 0.13;
-  public static double normalPower = 0.23;
-  public static double pitchTolerance = 2.5;
-}
