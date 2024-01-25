365ca914d570afc5943d3e5a5d485a74e5a0893a
Veerrohit Veeravadivel '26 <veerrohitv08@gmail.com>
2024-01-25 11:36:52 -0800

    activateserializer2

---
+++ src/main/java/frc/robot/subsystems/intake/ActivateSerializer2.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ActivateSerializer2 extends Command {
+  private IntakeSubsystem intakeSubsystem;
+
+  /** Creates a new ActivateSerializer. */
+  public ActivateSerializer2(IntakeSubsystem intakeSubsystem) {
+    this.intakeSubsystem = intakeSubsystem;
+    addRequirements(intakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intakeSubsystem.coastSerializer();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intakeSubsystem.setSerializerSpeed(-1);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intakeSubsystem.setSerializerSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
