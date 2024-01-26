0da5fa677cc50c8da96554f688987c7fba0a2984
KarthDaBoi <karthikeya.atmakuru@gmail.com>
2024-01-25 16:59:39 -0800

    MORE SYSiD FOR THE SWRIST
    LETS GO BOYZZ

--- src/main/java/frc/robot/subsystems/intakeWrist/IntakeWristSubsystem.java
+++ src/main/java/frc/robot/subsystems/intakeWrist/IntakeWristSubsystem.java
@@ -12,12 +12,24 @@ import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Measure;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.DeviceIDs.CanIds;
 //TODO: add setpoints
 import frc.robot.constants.RobotConstants.IntakeWristConstants;
+import static edu.wpi.first.units.MutableMeasure.mutable;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.RobotController;
 
 public class IntakeWristSubsystem extends SubsystemBase {
   private CANSparkMax wristMotor;
@@ -27,7 +39,48 @@ public class IntakeWristSubsystem extends SubsystemBase {
     wristMotor = new CANSparkMax(CanIds.wrist.id, MotorType.kBrushless);
     constraints = IntakeWristConstants.constraints;
     zeroEncoder();
-  }
+  } 
+  // mogus
+  private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
+  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+  private final MutableMeasure<Distance> m_distance = mutable(Meters.of(0));
+  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+  private final MutableMeasure<Velocity<Distance>> m_velocity = mutable(MetersPerSecond.of(0));
+
+  private final SysIdRoutine m_sysIdRoutine =
+    new SysIdRoutine(
+      // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
+      new SysIdRoutine.Config(),
+      new SysIdRoutine.Mechanism(
+        // Tell SysId how to plumb the driving voltage to the motors.
+        (Measure<Voltage> volts) -> {
+          wristMotor.setVoltage(volts.in(Volts));
+        },
+        // Tell SysId how to record a frame of data for each motor on the mechanism being
+        // characterized.
+        log -> {
+          // Record a frame for the left motors.  Since these share an encoder, we consider
+          // the entire group to be one motor.
+          log.motor("swrist")
+            .voltage(
+              m_appliedVoltage.mut_replace(
+                wristMotor.get() * RobotController.getBatteryVoltage(), Volts))
+            .linearPosition(m_distance.mut_replace(wristMotor.getEncoder().getPosition(), Meters))
+            .linearVelocity(
+              m_velocity.mut_replace(wristMotor.getEncoder().getVelocity(), MetersPerSecond));
+          // Record a frame for the right motors.  Since these share an encoder, we consider
+          // the entire group to be one motor.
+          // log.motor("shooter-top")
+          //   .voltage(
+          //     m_appliedVoltage.mut_replace(
+          //       shooterMotorTop.get() * RobotController.getBatteryVoltage(), Volts))
+          //   .linearPosition(m_distance.mut_replace(shooterMotorTop.getEncoder().getPosition(), Meters))
+          //   .linearVelocity(
+          //     m_velocity.mut_replace(shooterMotorTop.getEncoder().getVelocity(), MetersPerSecond));
+        },
+        // Tell SysId to make generated commands require this subsystem, suffix test state in
+        // WPILog with this subsystem's name ("drive")
+        this));
   
   public void setSpeed(double speed){
     wristMotor.set(speed);
