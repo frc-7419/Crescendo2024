7ef4b11dc78c69c5249926da445f31e9c6703f94
KarthDaBoi <karthikeya.atmakuru@gmail.com>
2024-02-22 17:08:28 -0800

    motion magic for shooter wrist

--- src/main/java/frc/robot/subsystems/shooterWrist/RaiseShooter.java
+++ src/main/java/frc/robot/subsystems/shooterWrist/RaiseShooter.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.shooterWrist;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
--- src/main/java/frc/robot/subsystems/shooterWrist/ShooterWrist.java
+++ src/main/java/frc/robot/subsystems/shooterWrist/ShooterWrist.java
@@ -4,18 +4,29 @@
 
 package frc.robot.subsystems.shooterWrist;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import static edu.wpi.first.units.Units.Minute;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.MutableMeasure.mutable;
 import frc.robot.constants.ArmConstants;
-import frc.robot.constants.FieldConstants;
-import frc.robot.constants.RobotConstants;
 import frc.robot.constants.DeviceIDs.CanIds;
+
 
 
 public class ShooterWrist extends SubsystemBase {
@@ -23,6 +34,35 @@ public class ShooterWrist extends SubsystemBase {
   private TalonFX armMotor;
   private DutyCycleOut dutyCycleOut;
   private DutyCycleEncoder encoder;
+  private final MutableMeasure<Voltage> appliedVoltage = mutable(Volts.of(0));
+  Velocity<Angle> RotationsPerMinute = Rotations.per(Minute);
+  private final SysIdRoutine shooterWristSysIdRoutine =
+    new SysIdRoutine(
+      // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
+      new SysIdRoutine.Config(),
+      new SysIdRoutine.Mechanism(
+        // Tell SysId how to plumb the driving voltage to the motors.
+        (Measure<Voltage> volts) -> {
+          armMotor.setVoltage(volts.in(Volts));
+        },
+        // Tell SysId how to record a frame of data for each motor on the mechanism being
+        // characterized.
+        log -> {
+          // Record a frame for the left motors.  Since these share an encoder, we consider
+          // the entire group to be one motor.
+          log.motor("shooter-wrist")
+            .voltage(
+              appliedVoltage.mut_replace(
+                armMotor.getDutyCycle().getValueAsDouble() * RobotController.getBatteryVoltage(), Volts))
+            // .angularPosition(shooterDistance)
+            .angularVelocity(RotationsPerMinute.of(getVelocity()))
+           .angularPosition(Rotations.of(getPosition()));
+          // Record a frame for the right motors.  Since these share an encoder, we consider
+          // the entire group to be one motor.
+        },
+        // Tell SysId to make generated commands require this subsystem, suffix test state in
+        // WPILog with this subsystem's name ("drive")
+        this));
 
     public ShooterWrist() {
     armMotor = new TalonFX(CanIds.shooterWrist.id, "Ryan Biggee");
@@ -32,6 +72,9 @@ public class ShooterWrist extends SubsystemBase {
     encoder = new DutyCycleEncoder(0 );
     encoder.setPositionOffset(ArmConstants.armOffset);
   }
+  public double getVelocity() {
+  return armMotor.getVelocity().getValueAsDouble() / 70;
+}
 
   public double rotationToRadians(double rotations){
     return rotations * 2 * Math.PI;
@@ -56,7 +99,32 @@ public class ShooterWrist extends SubsystemBase {
   public void setPower(double power){
     dutyCycleOut.Output = power;
     dutyCycleOut.EnableFOC = true;
-    armMotor.setControl(dutyCycleOut);
+    armMotor.setControl(dutyCycleOut);  
+  }
+
+  public void setMMConfigs() {
+  var talonFXConfigs = new TalonFXConfiguration();
+  var slot0Configs = talonFXConfigs.Slot0;
+  slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+  slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+  slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+  slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+  slot0Configs.kI = 0; // no output for integrated error
+  slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+  var motionMagicConfigs = talonFXConfigs.MotionMagic;
+  motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+  motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+  motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+  armMotor.getConfigurator().apply(talonFXConfigs);
+
+  }
+
+  public void applyRequest() {
+    final MotionMagicVoltage armRequest = new MotionMagicVoltage(0);
+    armMotor.setControl(armRequest.withPosition(100));
+
   }
   public void coast(){
     armMotor.setNeutralMode(NeutralModeValue.Coast);
