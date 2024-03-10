1eafdc2f09f3be84eea591b3ab1442cf3b30cecd
MostOriginalIGN <astrogamer543@gmail.com>
2024-03-09 22:23:11 -0800

    fix shoot note far

--- src/main/java/frc/robot/subsystems/shooterWrist/ShootNoteFar.java
+++ src/main/java/frc/robot/subsystems/shooterWrist/ShootNoteFar.java
@@ -25,14 +25,9 @@ public class ShootNoteFar extends SequentialCommandGroup {
   public ShootNoteFar(ShooterWrist shooterWrist, ShooterSubsystem shooterSubsystem, CommandSwerveDrivetrain drivetrain, IntakeSubsystem intakeSubsystem, double setpoint) {
     addCommands(
       new ParallelDeadlineGroup(
-        new SequentialCommandGroup(
-          new ParallelDeadlineGroup(
-            new RunSerializer(intakeSubsystem).withTimeout(0.5),
-            new RunShooter(shooterSubsystem, 0.6)
-          )
-      ),
-        new RaiseShooterWithPID(shooterWrist, setpoint)
-    )
+        new RunSerializer(intakeSubsystem).withTimeout(0.5),
+        new RaiseShooterWithPID(shooterWrist, setpoint) // this has around no effect but ig keep for validation.
+      )
     );
   }
 }
